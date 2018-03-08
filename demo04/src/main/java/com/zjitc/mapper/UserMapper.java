a13fe2a61b2a22910a5603fb2ea721ee7b663dc3
CrazyBoyKevin <kevin160718@163.com>
2018-03-08 16:47:23 +0800

    done demo04 and rename some files

---
+++ demo04/src/main/java/com/zjitc/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zjitc.mapper;
+
+import com.zjitc.pojo.User;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Create by IntelliJ IDEA
+ *
+ * @author: jsonor
+ * @date-Time: 2018/3/8 14:31
+ * @description:
+ */
+public interface UserMapper {
+
+  //@Select("SELECT * FROM users WHERE id = #{id}")
+  User findById(Integer id);
+}
---
+++ demo04/src/main/test/com/zjitc/UserMapperTest.java
@@ -0,0 +1,35 @@
+package com.zjitc;
+
+import com.zjitc.mapper.UserMapper;
+import com.zjitc.pojo.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Create by IntelliJ IDEA
+ *
+ * @author: jsonor
+ * @date-Time: 2018/3/8 15:10
+ * @description:
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:mybatis-spring-config.xml")
+public class UserMapperTest {
+
+  @Autowired
+  private UserMapper userMapper;
+
+  @Test
+  public void findById() {
+    User user = userMapper.findById(1);
+    Assert.assertEquals(1, user.getId().intValue());
+    Assert.assertEquals(1, user.getAge().intValue());
+    Assert.assertEquals("a",user.getName());
+  }
+
+}
